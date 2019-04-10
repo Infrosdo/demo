ca3470dfb513bdd02dde916ff2569d6c64a601a5
Huke <747711876@qq.com>
2019-04-10 15:55:12 +0800

    test1

--- src/main/java/JedisDemo.java
+++ src/main/java/JedisDemo.java
@@ -6,11 +6,14 @@ public class JedisDemo {
         //创建一个Jedis的连接
         Jedis jedis = new Jedis("127.0.0.1", 6379);
         //执行redis命令
-        jedis.set("key1", "hello world");
+        jedis.set("key2", "hello world");
         //从redis中取值
-        String result = jedis.get("key1");
+        String result2 = jedis.get("key2");
         //打印结果
-        System.out.println(result);
+        System.out.println(result2);
+
+        String result1 = jedis.get("key1");
+        System.out.println(result1);
         //关闭连接
         jedis.close();
 
